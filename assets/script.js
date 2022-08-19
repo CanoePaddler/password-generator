5eddd86c61174bcb52558fbd5f7c1539b24eb12e
Todd McBride <tmcbridejr@gmail.com>
2022-08-18 21:00:07 -0700

    forgot to link css and js - also accidentally deleted js scrips and added starter back in

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,11 @@
 // Assignment code here
-var length = Number(prompt('How long would you like your password to be? Please enter a number between 8 and 128...'))
-var type = prompt('Please enter any special character types you would like to have in your password...')
-var pass = generatePassword();
-document.getElementById('password').value = password;
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var charSets = {
-    lowercase: 'abcdefghijklmnopqrstuvwxyz',
-    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    numeric: '0123456789',
-    special: ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-  };
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
